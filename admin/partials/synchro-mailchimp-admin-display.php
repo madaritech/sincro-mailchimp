226a5b7c7f45aee1eb7db16ecc65dbd7931e141d
madaritech <dm@madaritech.com>
2017-10-14 15:04:55 +0200

    Refactoring based on WordPress Coding Standard

--- admin/partials/synchro-mailchimp-admin-display.php
+++ admin/partials/synchro-mailchimp-admin-display.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Provide a admin area view for the plugin
  *
@@ -12,186 +11,204 @@
  * @subpackage Synchro_Mailchimp/admin/partials
  * @author     Madaritech <freelance@madaritech.com>
  */
+
 ?>
 
 <br>
 <table>
-    <tr>
-        <td>
-            <img src="<?php echo plugins_url( '../images/logo_synchro_mailchimp.png', __FILE__ ); ?>" alt="Synchro MailChimp" height="120px">
-        </td>
-        <td>
-            <div style="font-size: 30px; font-weight: bold; margin-bottom: 10px; color: black;">&nbsp;<?php esc_attr_e( 'Synchro MailChimp Plugin', 'synchro_mailchimp' ); ?></div>
-            <div style="font-size: 14px; font-weight: bold;">&nbsp;&nbsp;by <a href="http://www.madaritech.com" target="_blank">Madaritech</a></div>
-        </td>
-    </tr>
+	<tr>
+		<td>
+			<img src="<?php echo esc_url( plugins_url( '../images/logo_synchro_mailchimp.png', __FILE__ ) ); ?>" alt="Synchro MailChimp" height="120px">
+		</td>
+		<td>
+			<div style="font-size: 30px; font-weight: bold; margin-bottom: 10px; color: black;">&nbsp;<?php esc_attr_e( 'Synchro MailChimp Plugin', 'synchro_mailchimp' ); ?></div>
+			<div style="font-size: 14px; font-weight: bold;">&nbsp;&nbsp;by <a href="http://www.madaritech.com" target="_blank">Madaritech</a></div>
+		</td>
+	</tr>
 </table>
 
 <div class="wrap">
 <h2></h2>
 
-<?php if ($save_settings) : ?>
+<?php if ( $save_settings ) : ?>
 
-    <div class="notice notice-success is-dismissible" >
-        <p><strong><?php _e( 'Configuration settings saved.', 'synchro_mailchimp' ); ?></strong></p>
-    </div>
+	<div class="notice notice-success is-dismissible" >
+		<p><strong><?php esc_html_e( 'Configuration settings saved.', 'synchro_mailchimp' ); ?></strong></p>
+	</div>
 
 <?php endif; ?>
 
-<?php if (!$this->requirements_service->mfw_is_missing()) : ?>
+<?php if ( ! $this->requirements_service->mfw_is_missing() ) : ?>
 
-    <div id="icon-options-general" class="icon32"></div>
-    <!--h1><?php esc_attr_e( 'Settings  ', 'synchro_mailchimp' ); ?></h1-->
+	<div id="icon-options-general" class="icon32"></div>
+	<!--h1><?php esc_attr_e( 'Settings  ', 'synchro_mailchimp' ); ?></h1-->
 
-    <div id="poststuff">
+	<div id="poststuff">
 
-        <div id="post-body" class="metabox-holder columns-2">
+		<div id="post-body" class="metabox-holder columns-2">
 
-            <!-- main content -->
-            <div id="post-body-content">
+			<!-- main content -->
+			<div id="post-body-content">
 
-                <div class="meta-box-sortables ui-sortable">
+				<div class="meta-box-sortables ui-sortable">
 
-                    <form method="POST">
-                        <input type="hidden" name="form_submitted" value="Y">
-
-<?php 
-
-foreach ($all_roles as $role => $role_val) {
-    $schema_name = $role . '_subscription_schema';
-    $name = $role_val['name'];
-
-?>
-
-                        <div class="postbox">
-
-                            <h3 class="hndle"><span style="font-size: 16px; font-weight: bold;"><?php esc_attr_e( $name, 'synchro_mailchimp' ); ?></span></h3>
-
-                            <div class="inside">
-
-                                <table style="width: 80%;text-align: center;border: 0 none" align="center">
-                                    <tbody>
-
-<?php 
-    //var_dump($settings_lists[$role]);
-    if (empty($settings_lists[$role])) 
-        _e('No Lists defined on MailChimp','synchro_mailchimp');
-    else {
-        foreach ($settings_lists[$role] as $list_id => $list_array) { 
-
-?>
-
-                                        <tr>
-                                            <td style="border: 0px solid #0073aa; padding: 2em 0">
-                                                <input name="<?php echo $role.'-list-'.$list_id; ?>" type="checkbox" id="" value="<?php echo $list_id; ?>"  <?php if ($list_array['checked']) echo " checked"; ?>/>
-                                                <span style="font-size: 15px; font-weight: bold;"><?php esc_attr_e( $list_array['name'], 'synchro_mailchimp' ); ?></span>
-                                            </td>
-                                            <td style="border: 1px solid #82878c;padding: 2em 0">
-
-<?php 
-            if (isset($settings_interest_categories[$role][$list_id])) {
-                foreach ($settings_interest_categories[$role][$list_id] as $category_id => $category_name) {
-                    echo '<div style="font-size: 14px; font-weight: bold; margin: 0 0 12px 0;">'.$category_name.'</span></div>';
-                    echo '<div style="margin: 0 0 12px 0;">';
-                    foreach ($settings_interests[$role][$category_id] as $interest_id => $interest_array) {
-?>
-
-                                                <input style="margin-left: 30px" name="<?php echo $role.'-list-'.$list_id.'-interest-'.$interest_id; ?>" type="checkbox" id="" value="<?php echo $interest_id; ?>" <?php if ($interest_array['checked']) echo " checked"; ?>/>
-                                                <span style="font-size: 14px" ><?php esc_attr_e( $interest_array['name'], 'synchro_mailchimp' ); ?></span>
+					<form method="POST">
+						<input type="hidden" name="form_submitted" value="Y">
 
 <?php
-                    }
-                    echo '</div>';
-                } 
-            } else {
-                _e('No interests defined on MailChimp','synchro_mailchimp');
-            }
-        }
+
+foreach ( $all_roles as $role => $role_val ) {
+	$schema_name = $role . '_subscription_schema';
+	$name = $role_val['name'];
+
 ?>
-                                            </td>
-                                        </tr>
-<?php   
-    }
+
+						<div class="postbox">
+
+							<h3 class="hndle"><span style="font-size: 16px; font-weight: bold;"><?php echo esc_attr( $name ); ?></span></h3>
+
+							<div class="inside">
+
+								<table style="width: 80%;text-align: center;border: 0 none" align="center">
+									<tbody>
+
+<?php
+if ( empty( $settings_lists[ $role ] ) ) {
+	esc_html_e( 'No Lists defined on MailChimp','synchro_mailchimp' );
+} else {
+	foreach ( $settings_lists[ $role ] as $list_id => $list_array ) {
+
 ?>
-                                    </tbody>
-                                </table>
-<?php if (!empty($settings_lists[$role])) : ?>
-                                <p align="right"><input class="button-primary" type="submit" name="salva" id="<?php echo $role; ?>-submit-button" value="<?php esc_attr_e( 'Save Settings', 'synchro_mailchimp' ); ?>" /></p>
+
+									<tr>
+										<td style="border: 0px solid #0073aa; padding: 2em 0">
+											<input name="<?php echo esc_attr( $role . '-list-' . $list_id ); ?>" type="checkbox" id="" value="<?php echo esc_attr( $list_id ); ?>"  
+																	<?php
+																	if ( $list_array['checked'] ) {
+																		echo ' checked';}
+?>
+/>
+											<span style="font-size: 15px; font-weight: bold;"><?php echo esc_attr( $list_array['name'] ); ?></span>
+										</td>
+										<td style="border: 1px solid #82878c;padding: 2em 0">
+
+<?php
+if ( isset( $settings_interest_categories[ $role ][ $list_id ] ) ) {
+	foreach ( $settings_interest_categories[ $role ][ $list_id ] as $category_id => $category_name ) {
+		echo '<div style="font-size: 14px; font-weight: bold; margin: 0 0 12px 0;">' . esc_attr( $category_name ) . '</span></div>';
+		echo '<div style="margin: 0 0 12px 0;">';
+		foreach ( $settings_interests[ $role ][ $category_id ] as $interest_id => $interest_array ) {
+?>
+
+									<input style="margin-left: 30px" name="<?php echo esc_attr( $role . '-list-' . $list_id . '-interest-' . $interest_id ); ?>" type="checkbox" id="" value="<?php echo esc_attr( $interest_id ); ?>" 
+																						<?php
+																						if ( $interest_array['checked'] ) {
+																							echo ' checked';}
+?>
+/>
+									<span style="font-size: 14px" ><?php echo esc_attr( $interest_array['name'] ); ?></span>
+
+<?php
+		}
+		echo '</div>';
+	}
+} else {
+	esc_attr_e( 'No interests defined on MailChimp','synchro_mailchimp' );
+}
+	}
+?>
+										</td>
+									</tr>
+<?php
+}
+?>
+									</tbody>
+								</table>
+<?php if ( ! empty( $settings_lists[ $role ] ) ) : ?>
+								<p align="right"><input class="button-primary" type="submit" name="salva" id="<?php echo esc_attr( $role ); ?>-submit-button" value="<?php esc_attr_e( 'Save Settings', 'synchro_mailchimp' ); ?>" /></p>
 <?php endif; ?>
-                            </div>
-                            <!-- .inside -->
+							</div>
+							<!-- .inside -->
 
-                        </div>
-                        <!-- .postbox -->
-<?php 
-} 
+						</div>
+						<!-- .postbox -->
+<?php
+}
 ?>
 
-                    </form>
-                </div>
-                <!-- .meta-box-sortables .ui-sortable -->
+					</form>
+				</div>
+				<!-- .meta-box-sortables .ui-sortable -->
 
-            </div>
-            <!-- post-body-content -->
+			</div>
+			<!-- post-body-content -->
 
-            <!-- sidebar -->
-            <div id="postbox-container-1" class="postbox-container">
+			<!-- sidebar -->
+			<div id="postbox-container-1" class="postbox-container">
 
-                <div class="meta-box-sortables">
+				<div class="meta-box-sortables">
 
-                    <div class="postbox">
+					<div class="postbox">
 
-                        <h2 class="hndle"><span><?php esc_attr_e(
-                                    'Instructions', 'synchro_mailchimp'
-                                ); ?></span></h2>
+						<h2 class="hndle"><span>
+						<?php
+						esc_attr_e(
+							'Instructions', 'synchro_mailchimp'
+						);
+								?>
+								</span></h2>
 
-                        <div class="inside">
-                            <p><?php esc_attr_e( 'This plugin let you synchronize the subscription of the WordPress users to MailChimp lists upon the selections made on this page for every role. This selection sets the desired match. Then, to apply your choose to a particular user, go on the WordPress user setting page and check the Synchro MailChimp select box.', 'synchro_mailchimp' ); ?></p>
-                            <p><?php esc_attr_e( 'Important: is not possibile to select interests without select the corresponding list. In this case the selection will be lost.' ); ?></p>
-                        </div>
-                        <!-- .inside -->
+						<div class="inside">
+							<p><?php esc_attr_e( 'This plugin let you synchronize the subscription of the WordPress users to MailChimp lists upon the selections made on this page for every role. This selection sets the desired match. Then, to apply your choose to a particular user, go on the WordPress user setting page and check the Synchro MailChimp select box.', 'synchro_mailchimp' ); ?></p>
+							<p><?php esc_attr_e( 'Important: is not possibile to select interests without select the corresponding list. In this case the selection will be lost.' ); ?></p>
+						</div>
+						<!-- .inside -->
 
-                    </div>
-                    <!-- .postbox -->
+					</div>
+					<!-- .postbox -->
 
-                    <div class="postbox">
+					<div class="postbox">
 
-                        <h2 class="hndle"><span><?php esc_attr_e(
-                                    'Want to contribute?', 'synchro_mailchimp'
-                                ); ?></span></h2>
+						<h2 class="hndle"><span>
+						<?php
+						esc_attr_e(
+							'Want to contribute?', 'synchro_mailchimp'
+						);
+								?>
+								</span></h2>
 
-                        <div class="inside">
-                            <p><?php esc_attr_e( 'This plugin is completely free. Help me to improve it and release new updated versions. If you have requests for features or bug fixing leave a message: ', 'synchro_mailchimp' ); ?><a href="http://www.madaritech.com/#menu-contact" target="_blank">Madaritech contact form</a></p>
+						<div class="inside">
+							<p><?php esc_attr_e( 'This plugin is completely free. Help me to improve it and release new updated versions. If you have requests for features or bug fixing leave a message: ', 'synchro_mailchimp' ); ?><a href="http://www.madaritech.com/#menu-contact" target="_blank">Madaritech contact form</a></p>
 
-                            <div align="center">
-                                <p>
-                                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                                    <input type="hidden" name="cmd" value="_s-xclick">
-                                    <input type="hidden" name="hosted_button_id" value="7F3RJK8PYECUY">
-                                    <input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online!">
-                                    <img alt="" border="0" src="https://www.paypalobjects.com/it_IT/i/scr/pixel.gif" width="1" height="1">
-                                    </form>
-                                </p>
-                            </div>
-                        </div>
-                        <!-- .inside -->
+							<div align="center">
+								<p>
+									<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+									<input type="hidden" name="cmd" value="_s-xclick">
+									<input type="hidden" name="hosted_button_id" value="7F3RJK8PYECUY">
+									<input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online!">
+									<img alt="" border="0" src="https://www.paypalobjects.com/it_IT/i/scr/pixel.gif" width="1" height="1">
+									</form>
+								</p>
+							</div>
+						</div>
+						<!-- .inside -->
 
-                    </div>
-                    <!-- .postbox -->
+					</div>
+					<!-- .postbox -->
 
-                </div>
-                <!-- .meta-box-sortables -->
+				</div>
+				<!-- .meta-box-sortables -->
 
-            </div>
-            <!-- #postbox-container-1 .postbox-container -->
+			</div>
+			<!-- #postbox-container-1 .postbox-container -->
 
 
-        </div>
-        <!-- #post-body .metabox-holder .columns-2 -->
+		</div>
+		<!-- #post-body .metabox-holder .columns-2 -->
 
-        <br class="clear">
-    </div>
-    <!-- #poststuff -->
+		<br class="clear">
+	</div>
+	<!-- #poststuff -->
 
 <?php endif; ?>
 
--- admin/partials/synchro-mailchimp-users-admin-display.php
+++ admin/partials/synchro-mailchimp-users-admin-display.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Provide a admin area view for the plugin
  *
@@ -11,30 +10,31 @@
  * @package    Synchro_Mailchimp
  * @subpackage Synchro_Mailchimp/admin/partials
  */
+
 ?>
 
-<h3><?php echo __( 'Synchro MailChimp Subscrition', 'synchro_mailchimp' ); ?></h3>
+<h3><?php esc_html_e( 'Synchro MailChimp Subscrition', 'synchro_mailchimp' ); ?></h3>
 		<table class="form-table">
 			<tr>
 				<th>
-					<label for="mc_subscribe"><?php echo __( 'Subscribe to Mailing List', 'synchro_mailchimp' ); ?></label>
+					<label for="mc_subscribe"><?php esc_html_e( 'Subscribe to Mailing List', 'synchro_mailchimp' ); ?></label>
 					<div id="spinner"></div>
 				</th>
-				<td id="sm_result"><h4><?php echo __( 'Success!', 'synchro_mailchimp' ); ?></h4></td>
+				<td id="sm_result"><h4><?php esc_html_e( 'Success!', 'synchro_mailchimp' ); ?></h4></td>
 				<td id="chk_block">
 					<input type="checkbox"
 						   class="regular-text ltr"
 						   id="mc_subscribe"
 						   name="mc_subscribe"
 							<?php checked( $checked,1 ); ?>
-						   title=<?php echo __( 'User subscription to MailChimp list/s', 'synchro_mailchimp' ); ?>
+						   title=<?php esc_html_e( 'User subscription to MailChimp list/s', 'synchro_mailchimp' ); ?>
 							<?php
 							if ( ! current_user_can( 'administrator' ) ) {
 								echo 'disabled';}
 ?>
 						   >
 					<p class="description">
-						<?php echo __( 'Select the checkbox to subscribe user to MailChimp mailig lists', 'synchro_mailchimp' ); ?>
+						<?php esc_html_e( 'Select the checkbox to subscribe user to MailChimp mailig lists', 'synchro_mailchimp' ); ?>
 					</p>
 				</td>
 			</tr>
